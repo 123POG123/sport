4e0c606f6ea08b217735f09f9d2ae8aa3d291c20
Pavel <vinogradovpavel32@gmail.com>
2021-03-01 10:54:40 +0300

    'comment'

--- sport/views.py
+++ sport/views.py
@@ -3,20 +3,25 @@ from django.views.generic import TemplateView, DeleteView, CreateView, UpdateVie
 from .form import FormMyForm
 
 class HomePageView(TemplateView):
+    #home page
     template_name = 'sport/home.html'
 
 
 class AboutPageView(TemplateView):
+    #about page
     template_name = 'sport/about_us.html'
 
 
 class BlogPageView(TemplateView):
+    #blog page
     template_name = 'sport/blog.html'
 
 
 class ContactPageView(TemplateView):
+    #contact page
     template_name = 'sport/contact.html'
 
 class ServicesPageView(TemplateView):
+    #services page
     template_name = 'sport/services.html'
 
