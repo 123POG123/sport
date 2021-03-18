2063e74f2fa2610ffd94aa43f4e1c2c0742d6d96
Pavel <vinogradovpavel32@gmail.com>
2021-03-18 06:57:49 +0300

    'www'

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from firstProject import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', include('market.urls')),
 ]
 # if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT),
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT),
+#     urlpatterns += [
+#         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+#         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     ]
