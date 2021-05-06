ac9555bbe6b677b2b7e912831a6369924650b9d0
Pavel <45313368+123POG123@users.noreply.github.com>
2021-05-06 05:49:53 +0300

    Update urls.py

--- sport/urls.py
+++ sport/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, BlogPageView, ContactPageView, ServicesPageView
 
 urlpatterns = [
-    path('home', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
     path('blog/', BlogPageView.as_view(), name='blog'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
